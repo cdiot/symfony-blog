f771b99320967b0280df89ff796607bae67804ea
cdiot <christopher.diot5@gmail.com>
2022-11-27 16:07:58 +0100

    Add ArticleCategory entity + test

---
+++ src/Entity/ArticleCategory.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleCategoryRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticleCategoryRepository::class)]
+class ArticleCategory
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 55)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 110)]
+    private ?string $slug = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function __toString(): string
+    {
+        return $this->name;
+    }
+}
