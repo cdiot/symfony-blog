444d7fd06fb3a279b0f0eb5edfe7d3a18dc63a21
cdiot <christopher.diot5@gmail.com>
2022-11-27 13:54:25 +0100

    Add RegistrationController url smoke test

--- tests/Controller/SecurityControllerTest.php
+++ tests/Controller/SecurityControllerTest.php
@@ -13,7 +13,7 @@ class SecurityControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/connexion');
 
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Connecte toi');
+        $this->assertSelectorTextContains('h1', 'CONNECTE TOI');
     }
 
     public function testVisitingWhileLoggedIn()
