db0980f8701ed16a399d73e5f2177179b81cad57
cdiot <christopher.diot5@gmail.com>
2022-12-01 10:41:59 +0100

    Add Comment controller + template

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,3 +18,67 @@ new ResizeObserver(entries => {
     navigationMenubar.style.transition = "none"
   }
 }).observe(document.body)
+
+window.onload = () => {
+  let platform = document.querySelector("#article_form_platform");
+
+  platform.addEventListener("change", function () {
+      let form = this.closest("form")
+      let data = this.name + "=" + this.value
+
+      fetch(form.action, {
+          method: form.getAttribute("method"),
+          body: data,
+          headers: {
+              "Content-Type": "application/x-www-form-urlencoded; charset:UTF-8"
+          }
+      })
+      .then(response => response.text())
+      .then(html => {
+          let content = document.createElement("html")
+          content.innerHTML = html
+          let newSelect = content.querySelector("#article_form_game")
+          document.querySelector("#article_form_game").replaceWith(newSelect)
+      })
+  })
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+  handleCommentForm();
+});
+
+const handleCommentForm = () => {
+  if (null === document.querySelector('.comment-area')) {
+      return;
+  }
+
+  const commentForm = document.querySelector('form.comment-form');
+
+  commentForm.addEventListener('submit', async (e) => {
+      e.preventDefault();
+
+      const response = await fetch('/commentaires', {
+          headers: {
+              'X-Requested-With': 'XMLHttpRequest'
+          },
+          method: 'POST',
+          body: new FormData(e.target)
+      });
+
+      if (!response.ok) {
+          return;
+      }
+
+      const json = await response.json();
+
+      if (json.code === 'COMMENT_ADDED_SUCCESSFULLY') {
+          const commentsList = document.querySelector('.comment-list');
+          const commentCount = document.querySelector('.comment-count');
+          const commentFormContent = document.querySelector('#comment_form_content');
+          commentsList.insertAdjacentHTML('beforeend', json.message);
+          commentsList.lastElementChild.scrollIntoView();
+          commentCount.innerText = json.numberOfComments;
+          commentFormContent.value = '';
+      }
+  })
+}
