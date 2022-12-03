95d46295bc6b800fb9d6e0660361a9c3273f462f
cdiot <christopher.diot5@gmail.com>
2022-12-03 11:51:47 +0100

    Add Welcome Service

---
+++ src/Service/Welcome.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Service;
+
+class Welcome
+{
+    const SITE_INSTALLED_LABEL = 'Site installé';
+    const SITE_INSTALLED_NAME = 'symfony_blog_installed';
+
+    private ?string $username;
+
+    private ?string $email;
+
+    private ?string $password;
+
+    /**
+     * @return string|null
+     */
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string|null $username
+     */
+    public function setUsername(?string $username): void
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string|null $email
+     */
+    public function setEmail(?string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string|null $password
+     */
+    public function setPassword(?string $password): void
+    {
+        $this->password = $password;
+    }
+}
